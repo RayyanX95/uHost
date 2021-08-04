b54a852e86c4470c849475a2a19f1d6b7d6689d6
Ibrahim AlRayyan <ibrahim.alrayany@gmail>
2021-08-04 21:18:49 +0200

    use javascript to open and close mobile-nav

--- shared.js
+++ shared.js
@@ -1,7 +1,9 @@
 const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 const selectPlanButtons = document.querySelectorAll('.plan button');
-const noButton = document.querySelector('.modal__action--negative')
+const noButton = document.querySelector('.modal__action--negative');
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener('click', () => {
@@ -13,9 +15,15 @@ for (let i = 0; i < selectPlanButtons.length; i++) {
 backdrop.addEventListener('click', () => {
   modal.style.display = 'none';
   backdrop.style.display = 'none';
-})
+  mobileNav.style.display = 'none';
+});
 
 noButton.addEventListener('click', () => {
   modal.style.display = 'none';
   backdrop.style.display = 'none';
+});
+
+toggleButton.addEventListener('click', () => {
+  mobileNav.style.display = 'block';
+  backdrop.style.display = 'block';
 })
