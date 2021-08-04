526d703e3c859494cec50931c8be54f547e2ddbc
Ibrahim AlRayyan <ibrahim.alrayany@gmail>
2021-08-04 20:28:58 +0200

    use javaScript to dismiss the modal and the backdrop

--- shared.js
+++ shared.js
@@ -1,10 +1,21 @@
 const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 const selectPlanButtons = document.querySelectorAll('.plan button');
+const noButton = document.querySelector('.modal__action--negative')
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener('click', () => {
     modal.style.display = 'block';
     backdrop.style.display = 'block';
   });
-}
+};
+
+backdrop.addEventListener('click', () => {
+  modal.style.display = 'none';
+  backdrop.style.display = 'none';
+})
+
+noButton.addEventListener('click', () => {
+  modal.style.display = 'none';
+  backdrop.style.display = 'none';
+})
